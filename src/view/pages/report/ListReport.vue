ff7983f0efa8945f56bca79e9fd9419165bc94c1
solihin-interconnectdata <solihin@interconnectdata.com>
2022-12-20 09:02:41 +0700

    report

---
+++ src/view/pages/report/ListReport.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div
+        class="mb-5 d-flex flex-wrap justify-content-end align-items-end md:flex-row md:justify-content-between"
+      ></div>
+    </div>
+  </div>
+</template>
